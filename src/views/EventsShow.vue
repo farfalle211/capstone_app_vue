67e4700796583c9dda71a165727ba0c76cf34f1a
farfalle211 <farfalle211@gmail.com>
2019-03-14 02:05:43 -0500

    changed events show

--- src/views/EventsShow.vue
+++ src/views/EventsShow.vue
@@ -2,21 +2,22 @@
   <div class="container">
     <h1>{{ event.name }}</h1>
     <div>
-      Name: <input type="text" v-model="newPhotoName">
+    <!--   Name: <input type="text" v-model="newPhotoName">
       Date: <input type="text" v-model="newPhotoWidth">
       Category: <input type="text" v-model="newPhotoHeight">
-      <button v-on:click="createPhoto()">Create Photo</button>
+      <button v-on:click="createPhoto()">Create Photo</button> -->
     
 
 
     <h1>All Groups</h1>
-    <div v-for="group in event.groups">
-      <h2>{{ group.label }}</h2>
+      <div v-for="group in event.groups">
+      <button v-on:click="userEvent()">Show Groups</button>
+          <h2>{{ group.label }}</h2>
+        <div v-if="user_event_checker">
 <!--       <img v-bind:src="photo.url"> -->
-      <button v-on:click="showGroup(group)">Show Groups</button>
-      <div v-if="currentGroup === group">
-        <p>Width: {{ group.width }}</p>
-        <p>Height: {{ group.height }}</p>
+          <p>Size: {{ group.size }}</p>
+          <p>Location: {{ group.location }}</p>
+        </div>
       </div>
     </div>
   </div>
@@ -50,36 +51,57 @@ export default {
                       }
                       ]
               },
+      user_events: [],
+      user_event_checker: ""
     };
   },
   created: function() {
-        axios.get("/api/events/" + this.$route.params.id)
-        .then(response => {
+      axios.get("/api/events/" + this.$route.params.id)
+      .then(response => {
       this.event = response.data;
     });
 
+      axios.get("/api/user_events/")
+      .then(response => {
+        console.log(response.data);
+      this.user_events = response.data;
+    });
+
+
   },
   methods: {
-    createPhoto: function() {
-      var params = {
-        name: this.newPhotoName,
-        width: this.newPhotoWidth,
-        height: this.newPhotoHeight
-      };
-      axios.post("/api/photos", params).then(response => {
-        this.photos.push(response.data);
-        this.newPhotoName = "";
-        this.newPhotoWidth = "";
-        this.newPhotoHeight = "";
+    // createPhoto: function() {
+    //   var params = {
+    //     name: this.newPhotoName,
+    //     width: this.newPhotoWidth,
+    //     height: this.newPhotoHeight
+    //   };
+    //   axios.post("/api/photos", params).then(response => {
+    //     this.photos.push(response.data);
+    //     this.newPhotoName = "";
+    //     this.newPhotoWidth = "";
+    //     this.newPhotoHeight = "";
+    //   });
+    // },
+
+    userEvent: function() {
+      var identity = this.event.id
+      console.log(identity);
+      this.user_events.forEach(function(element) {
+        console.log(element);
+        if (element.event_id === identity) {
+          this.user_event_checker = true;
+        }
       });
-    },
-    showGroup: function(photo) {
-      if (this.currentPhoto === photo) {
-        this.currentPhoto = {};
-      } else {
-        this.currentPhoto = photo;
-      }
     }
+
+    // showGroup: function(photo) {
+    //   if (this.currentPhoto === photo) {
+    //     this.currentPhoto = {};
+    //   } else {
+    //     this.currentPhoto = photo;
+    //   }
+    // }
   }
 };
 </script>
